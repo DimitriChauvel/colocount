4d8cffea6ff7338e87fe96fee1b2f25f18bafb06
constans <gustave.constans@fairlymade.com>
2023-01-10 16:02:44 +0100

    feature/components-button-nav-logout

---
+++ frontend/src/components/input/input.js
@@ -0,0 +1,13 @@
+import { React } from "react";
+import "./input.css";
+
+function Input(props) {
+  return (
+    <div class="Input">
+      <input placeholder={props.name}></input>
+    </div>
+  );
+}
+
+export default Input;
+s;
