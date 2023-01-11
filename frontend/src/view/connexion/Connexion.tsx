6128e3651507ab53ed6092eb504639dc8a2ed970
constans <gustave.constans@fairlymade.com>
2023-01-11 15:37:44 +0100

    feature views login-regiser

--- frontend/src/view/connexion/Connexion.tsx
+++ frontend/src/view/connexion/Connexion.tsx
@@ -1,11 +1,73 @@
-import React from 'react';
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import "./Connexion.css";
 
-const connexion = () => {
-    return (
-        <div>
-            connexion
+import Button from "../../components/button/button";
+import Input from "../../components/input/input";
+
+const errEmail = () => {};
+
+const errPassword = () => {};
+
+function Login() {
+  const navigate = useNavigate();
+
+  const [errEmail, seterrEmail] = useState([]);
+  const [errPassword, seterrPassword] = useState([]);
+
+  const [state, setState] = useState({
+    email: "",
+    password: "",
+  });
+
+  const handleChange = (
+    event: React.ChangeEvent<HTMLInputElement>,
+    key: keyof typeof state
+  ) => {
+    setState({ ...state, [key]: event.target.value });
+  };
+
+  const handleClickRegister = (event: React.MouseEvent<HTMLButtonElement>) => {
+    navigate("/createaccount");
+  };
+
+  function connect() {
+    //En cours Attente de l'API
+    sessionStorage.setItem("user", state.email);
+  }
+
+  return (
+    <div id="login">
+      <div className="flex justify-end m-4">
+        <Button name="Register" onClick={handleClickRegister} />
+      </div>
+      <div className="flex flex-col gap-4 items-center container-login">
+        <h1>Login</h1>
+        <div className="flex flex-col gap-2">
+          <Input
+            onChange={(event) => handleChange(event, "email")}
+            placeholder="Email"
+            type="text"
+            required={true}
+          />
+          <Input
+            onChange={(event) => handleChange(event, "password")}
+            placeholder="Password"
+            type="password"
+            required={true}
+          />
         </div>
-    );
-};
 
-export default connexion;
+        <div>
+          <Button name="Sign in" onClick={connect} />
+        </div>
+        <div id="error">
+          {errEmail}
+          {errPassword}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Login;
--- frontend/src/view/createaccount/CreateAccount.tsx
+++ frontend/src/view/createaccount/CreateAccount.tsx
@@ -1,11 +1,76 @@
-import React from 'react';
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import { useState } from "react";
+import "./CreateAccount.css";
 
-const CreateAccount = () => {
-    return (
-        <div>
-            createaccounds
+import Button from "../../components/button/button";
+import Input from "../../components/input/input";
+
+function CreateAccount() {
+  const navigate = useNavigate();
+
+  const [state, setState] = useState({
+    email: "",
+    password: "",
+    firstName: "",
+    lastName: "",
+  });
+
+  const handleChange = (
+    event: React.ChangeEvent<HTMLInputElement>,
+    key: keyof typeof state
+  ) => {
+    setState({ ...state, [key]: event.target.value });
+  };
+
+  const handleClickLogin = (event: React.MouseEvent<HTMLButtonElement>) => {
+    navigate("/connexion");
+  };
+  function register() {
+    sessionStorage.setItem("user", state.email);
+  }
+  return (
+    <div>
+      {" "}
+      <div className="flex justify-end m-4">
+        <Button name="Login" onClick={handleClickLogin} />
+      </div>
+      <div className="flex flex-col gap-4 items-center container-register">
+        <h1>Register</h1>
+        <div className="flex flex-col gap-2">
+          <Input
+            onChange={(event) => handleChange(event, "firstName")}
+            placeholder="First Name"
+            type="text"
+            required={true}
+            key="firstName"
+          />
+          <Input
+            onChange={(event) => handleChange(event, "lastName")}
+            placeholder="Last Name"
+            type="text"
+            required={true}
+          />
+          <Input
+            onChange={(event) => handleChange(event, "email")}
+            placeholder="Email"
+            type="text"
+            required={true}
+          />
+          <Input
+            onChange={(event) => handleChange(event, "password")}
+            placeholder="Password"
+            type="password"
+            required={true}
+          />
         </div>
-    );
-};
+
+        <div>
+          <Button name="Register" onClick={register} />
+        </div>
+      </div>
+    </div>
+  );
+}
 
 export default CreateAccount;
