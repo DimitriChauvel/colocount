31eccaca451d983cb003ea3d44489e64c82ff6e6
constans <gustave.constans@fairlymade.com>
2023-01-11 10:47:53 +0100

    update disgn nav  - button

--- frontend/src/components/button/button.js
+++ frontend/src/components/button/button.js
@@ -3,7 +3,7 @@ import "./button.css";
 
 function Button(props) {
   return (
-    <button className="py-2 px-4 bg-blue-500 text-white font-semibold rounded-lg shadow-md hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-400 focus:ring-opacity-75">
+    <button className="py-2 px-4 orange-bg text-white font-semibold rounded-md shadow-md hover:bg-orange-700 focus:outline-none focus:ring-2 focus:ring-orange-400 focus:ring-opacity-75">
       {props.name}
     </button>
   );
--- frontend/src/components/buttonCross/buttonCross.js
+++ frontend/src/components/buttonCross/buttonCross.js
@@ -5,7 +5,7 @@ import Logo from "../../assets/icons/cancelCross.svg";
 function ButtonCross(props) {
   return (
     <div>
-      <button className="py-2 px-2 bg-blue-500 text-white font-semibold rounded-lg shadow-md hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-400 focus:ring-opacity-75">
+      <button className="py-2 px-2 orange-bg text-white font-semibold rounded-md shadow-md hover:bg-orange-700 focus:outline-none focus:ring-2 focus:ring-orange-400 focus:ring-opacity-75">
         <img src={Logo} className="h-4 w-4" />
       </button>
     </div>
