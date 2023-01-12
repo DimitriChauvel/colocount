68f40deb70ba16158c2ebf1c9aab105a4e6c78d6
cyprien cazenave <33652607+CyprienCazenave@users.noreply.github.com>
2023-01-12 11:34:58 +0100

    Revert "Backend entity"

---
+++ backend/db/dump.sql
@@ -0,0 +1,73 @@
+CREATE TABLE `User` (
+                        `id` VARCHAR(255) NOT NULL AUTO_INCREMENT UNIQUE,
+                        `firstname` VARCHAR(255) NOT NULL,
+                        `lastname` VARCHAR(255) NOT NULL,
+                        `email` VARCHAR(255) NOT NULL UNIQUE,
+                        `phone` VARCHAR(255) UNIQUE,
+                        `profile_picture` VARCHAR(255) NOT NULL,
+                        `password` VARCHAR(255) NOT NULL,
+                        PRIMARY KEY (`id`)
+);
+
+CREATE TABLE `Flatshare` (
+                             `id` VARCHAR(255) NOT NULL UNIQUE,
+                             `banner_picture` VARCHAR(255) NOT NULL,
+                             `name` VARCHAR(255) NOT NULL,
+                             PRIMARY KEY (`id`)
+);
+
+CREATE TABLE `UserToFlatshare` (
+                                   `id` VARCHAR(255) NOT NULL UNIQUE,
+                                   `user_id` VARCHAR(255) NOT NULL,
+                                   `flatshare_id` VARCHAR(255) NOT NULL,
+                                   `role` VARCHAR(255) NOT NULL DEFAULT 'due',
+                                   `due` FLOAT NOT NULL DEFAULT '0',
+                                   PRIMARY KEY (`id`)
+);
+
+CREATE TABLE `Expense` (
+                           `id` VARCHAR(255) NOT NULL UNIQUE,
+                           `name` VARCHAR(255) NOT NULL,
+                           `paying_one_id` VARCHAR(255) NOT NULL,
+                           `flatshare_id` VARCHAR(255) NOT NULL,
+                           `sum` FLOAT NOT NULL,
+                           `category_id` VARCHAR(255) NOT NULL,
+                           PRIMARY KEY (`id`)
+);
+
+CREATE TABLE `Category` (
+                            `id` VARCHAR(255) NOT NULL UNIQUE,
+                            `name` VARCHAR(255) NOT NULL UNIQUE,
+                            `default` BOOLEAN NOT NULL DEFAULT false,
+                            PRIMARY KEY (`id`)
+);
+
+CREATE TABLE `Task` (
+                        `id` VARCHAR(255) NOT NULL UNIQUE,
+                        `name` VARCHAR(255) NOT NULL UNIQUE,
+                        `flatshare_id` VARCHAR(255) NOT NULL,
+                        PRIMARY KEY (`id`)
+);
+
+
+CREATE TABLE `FlatshareToCategory` (
+                                       `id` VARCHAR(255) NOT NULL,
+                                       `flatshare_id` VARCHAR(255) NOT NULL,
+                                       `category_id` VARCHAR(255) NOT NULL
+);
+
+ALTER TABLE `UserToFlatshare` ADD CONSTRAINT `UserToFlatshare_fk0` FOREIGN KEY (`user_id`) REFERENCES `User`(`id`);
+
+ALTER TABLE `UserToFlatshare` ADD CONSTRAINT `UserToFlatshare_fk1` FOREIGN KEY (`flatshare_id`) REFERENCES `Flatshare`(`id`);
+
+ALTER TABLE `Expense` ADD CONSTRAINT `Expense_fk0` FOREIGN KEY (`paying_one_id`) REFERENCES `User`(`id`);
+
+ALTER TABLE `Expense` ADD CONSTRAINT `Expense_fk1` FOREIGN KEY (`flatshare_id`) REFERENCES `Flatshare`(`id`);
+
+ALTER TABLE `Expense` ADD CONSTRAINT `Expense_fk2` FOREIGN KEY (`category_id`) REFERENCES `Category`(`id`);
+
+ALTER TABLE `Task` ADD CONSTRAINT `Task_fk0` FOREIGN KEY (`flatshare_id`) REFERENCES `Flatshare`(`id`);
+
+ALTER TABLE `FlatshareToCategory` ADD CONSTRAINT `FlatshareToCategory_fk0` FOREIGN KEY (`flatshare_id`) REFERENCES `Flatshare`(`id`);
+
+ALTER TABLE `FlatshareToCategory` ADD CONSTRAINT `FlatshareToCategory_fk1` FOREIGN KEY (`category_id`) REFERENCES `Category`(`id`);
--- frontend/src/components/block-title/BlockTitle.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function BlockTitle(props: any) {
-  return (
-    <div className="w-80 py-2 px-4 text-center bg-blue text-white font-semibold rounded-md shadow-md focus:outline-none focus:ring-2 focus:ring-orange focus:ring-opacity-75">
-      {props.name}
-    </div>
-  );
-}
