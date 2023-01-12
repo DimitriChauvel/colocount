9cdcec72f504f9b2018567db49bb840bbea0398d
Cyprien <cyprien.cazenave@gmail.com>
2023-01-12 11:27:22 +0100

    backend_entity

--- backend/src/Model/Entity/Flatshare.php
+++ backend/src/Model/Entity/Flatshare.php
@@ -7,4 +7,60 @@ class Flatshare extends BaseEntity
     private string $id;
     private string $banner_picture;
     private string $name;
+
+    /**
+     * @return string
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param string $id
+     * @return Flatshare
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getBannerPicture()
+    {
+        return $this->banner_picture;
+    }
+
+    /**
+     * @param string $banner_picture
+     * @return Flatshare
+     */
+    public function setBannerPicture($banner_picture)
+    {
+        $this->banner_picture = $banner_picture;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param string $name
+     * @return Flatshare
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+        return $this;
+    }
+
+
 }
--- backend/src/Model/Entity/Task.php
+++ backend/src/Model/Entity/Task.php
@@ -8,4 +8,58 @@ class Task extends BaseEntity
     private string $name;
     private string $flatshare_id;
 
+    /**
+     * @return string
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param string $id
+     * @return Task
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param string $name
+     * @return Task
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getFlatshareId()
+    {
+        return $this->flatshare_id;
+    }
+
+    /**
+     * @param string $flatshare_id
+     * @return Task
+     */
+    public function setFlatshareId($flatshare_id)
+    {
+        $this->flatshare_id = $flatshare_id;
+        return $this;
+    }
+
 }
