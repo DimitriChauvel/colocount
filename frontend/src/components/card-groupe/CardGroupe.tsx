bad8beace3208a802cc7842d6f72d19f52ac9c1b
M3dzZ <mehdichaibi05@gmail.com>
2023-01-12 14:57:03 +0100

    prepare get data flatshare

--- frontend/src/components/card-groupe/CardGroupe.tsx
+++ frontend/src/components/card-groupe/CardGroupe.tsx
@@ -1,9 +1,27 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useState } from "react";
 
 export default function CardGroupe() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetch("api/post/groupe")
+      .then((response) => response.json())
+      .then(setData);
+  }, []);
+
   return (
-    <button className="w-80 h-32 py-2 px-4 text-center text-white font-semibold rounded-md shadow-md focus:outline-none hover:ring-2 hover:ring-blue focus:ring-opacity-75">
-      ygywgsygwysgwysgywgsw
-    </button>
+    <div>
+      {data.map((element) => (
+        <button
+          // key={element.id}
+          className="w-80 h-32 py-2 px-4 text-center text-white font-semibold rounded-md shadow-md focus:outline-none hover:ring-2 hover:ring-blue focus:ring-opacity-75"
+        >
+          {/* {element.name}
+          {element.datetime}
+          {element.banner_picture} */}
+        </button>
+      ))}
+    </div>
   );
 }
