3a1c385f58138398e53a24b329f258b764f9028b
juroche42 <94986757+juroche42@users.noreply.github.com>
2023-01-13 14:11:58 +0100

    Refractor Controller

--- backend/src/Controller/UserController.php
+++ backend/src/Controller/UserController.php
@@ -12,44 +12,34 @@ class UserController extends AbstractController
     public function getAllUsers() {
         $users = new UserManager(new PDOFactory());
         $data = $users->getAll();
-
-        header('Content-type: application/json');
-        echo json_encode($data);
+        $this->renderJSON($data);
     }
 
     #[Route('/users/id/{id}', name: "get_one_user", methods: ["GET"])]
     public function getOneUser(string $id) {
         $users = new UserManager(new PDOFactory());
         $data = $users->getOne($id);
-
-        header('Content-type: application/json');
-        echo json_encode($data);
+        $this->renderJSON($data);
     }
 
     #[Route('/users/email/{email}', name: "get_user_by_email", methods: ["GET"])]
     public function getUserByEmail(string $email) {
         $users = new UserManager(new PDOFactory());
         $data = $users->getByEmail($email);
-
-        header('Content-type: application/json');
-        echo json_encode($data);
+        $this->renderJSON($data);
     }
 
     #[Route('/users', name: "post_one_user", methods: ["POST"])]
     public function postOneUser() {
         $users = new UserManager(new PDOFactory());
         $data = $users->postOne();
-
-        header('Content-type: application/json');
-        echo json_encode($data);
+        $this->renderJSON($data);
     }
 
     #[Route('/users', name: "put_one_user", methods: ["PUT"])]
     public function putOneUser() {
         $users = new UserManager(new PDOFactory());
         $data = $users->putOne();
-
-        header('Content-type: application/json');
-        echo json_encode($data);
+        $this->renderJSON($data);
     }
 }
