c5527e63f5ed6bd39c66a9a9ed814ad50b660eb3
juroche42 <94986757+juroche42@users.noreply.github.com>
2023-01-15 17:53:13 +0100

    Test userToFlatshare

--- backend/src/Controller/UserToFlatshareController.php
+++ backend/src/Controller/UserToFlatshareController.php
@@ -15,21 +15,21 @@ class UserToFlatshareController extends AbstractController
         $this->renderJSON($data);
     }
 
-    #[Route('/UserToFlatshare/user/{id}', name: "get_UserToFlatshare_by_user_id", methods: ["GET"])]
+    #[Route('/userToFlatshare/user/{id}', name: "get_UserToFlatshare_by_user_id", methods: ["GET"])]
     public function getByUser(string $id) {
         $userToFlatshare = new UserToFlatshareManager(new PDOFactory());
         $data = $userToFlatshare->getByUser($id);
         $this->renderJSON($data);
     }
 
-    #[Route('/UserToFlateshare', name: "post_one_UserToFlatshare", methods: ["POST"])]
+    #[Route('/userToFlatshare', name: "post_one_UserToFlatshare", methods: ["POST"])]
     public function postOneUserToUserToFlatshare() {
         $userToFlatShare = new UserToFlatshareManager(new PDOFactory());
         $data = $userToFlatShare->postOne();
         $this->renderJSON($data);
     }
 
-    #[Route('/UserToFlatshare/{id}', name: "delete_one_UserToFlatshare", methods: ["DELETE"])]
+    #[Route('/userToFlatshare/{id}', name: "delete_one_UserToFlatshare", methods: ["DELETE"])]
     public function deleteOneUserToFlatshare(string $id) {
         $userToFlatshare = new UserToFlatshareManager(new PDOFactory());
         $userToFlatshare->deleteOne($id);
