c31ab87483fb3725c202f3d407cdb01a7dd06e22
Cyprien <cyprien.cazenave@gmail.com>
2023-01-15 17:05:29 +0100

    add calcul function

--- backend/src/Controller/ExpenseController.php
+++ backend/src/Controller/ExpenseController.php
@@ -5,6 +5,8 @@ namespace App\Controller;
 use App\Model\Factory\PDOFactory;
 use App\Route\Route;
 use App\Model\Manager\ExpenseManager;
+use App\Model\Manager\UserManager;
+use App\Model\Manager\UserToExpenseManager;
 
 class ExpenseController extends AbstractController
 {
@@ -33,7 +35,54 @@ class ExpenseController extends AbstractController
     public function postOneExpense() {
         $expenses = new ExpenseManager(new PDOFactory());
         $data = $expenses->postOne();
+        $response = (array) json_decode(file_get_contents('php://input'));
+        $currentUser = $this->getByUser($id);
+        $participants = $response['participants'];
+        $participants[] = $currentUser;
+        $nbParticipants = count($participants);
+        $sumExpense = $response['sumExpense'];
+        $duePerParticipants = -1 * floor(100 * $sumExpense % $nbParticipants)%100;
+        $dueLastParticipant = $sumExpense - $duePerParticipants * $nbParticipants;
+
         $this->renderJSON($data);
+    }
+
+    #[Route('expense', name: "expenseCalcul", methods: ["GET"])]
+    public function expenseCalcul(string $id){
+        $userManager = new UserManager(new PDOFactory());
+        $users = $this->getByUser($id);
+        $expenseManager = new ExpenseManager(new PDOFactory());
+        $expense = [];
+        $data = $userManager->getOne($id);
+
+        foreach ($users as $user) {
+            $result = $userManager->getByUserId($id);
+            $payback = count($result);
+            if ($payback > 0) {
+                $expense[$result[0]] = $result[0];
+            }
+
+        }
+        while ($payback != 0) {
+            asort($participants);
+            $absoluteValue = $participants[0];
+            $paybackValue = min(abs($absoluteValue),$participants[-1]);
+            $participants[0] -= $paybackValue;
+            $participants[-1] -= $paybackValue;
+            $owes[] = array("sender" => key($participants[0]),
+                "receiver" => key($participants[-1]),
+                "value" => $paybackValue);
+            if ($participants[0] === 0){
+                unset($participants[0]);
+            }
+            if ($participants[-1] === 0){
+                unset($participants[-1]);
+            }
+            $paybackEnd = count($participants);
+        }
+
+        $this->renderJSON($data);
+
     }
 
     #[Route('/expense/{id}', name: "delete_one_expense", methods: ["DELETE"])]
