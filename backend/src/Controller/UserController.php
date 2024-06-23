610cc8d1765468e705ae924f83e9abb3810ee564
juroche42 <julesro42@gmail.com>
2024-06-23 15:42:13 +0200

    update user

--- backend/src/Controller/UserController.php
+++ backend/src/Controller/UserController.php
@@ -108,4 +108,44 @@ class UserController extends AbstractController
         $this->renderJSON($user);
         die();
     }
+
+    #[Route('/profil', name: "update_profil", methods: ["PUT"])]
+    public function updateProfil() {
+        $response = json_decode(file_get_contents('php://input'), true);
+        
+        $userId = $this->checkJwtAndGetUser();
+        $userManager = new UserManager(new PDOFactory());
+        $user = $userManager->getOne($userId);
+        if (!$user) {
+            $this->renderJSON(['error' => 'User not found'], 404);
+            die();
+        }
+        if (isset($response['email'])) {
+            $email = $response['email'];
+            if ($userManager->getByEmail($email)) {
+                $this->renderJSON(['error' => 'Email already in use'], 400);
+                die();
+            }
+            $user->setEmail($email);
+        }
+        if (isset($response['oldPassword']) && isset($response['newPassword'])) {
+            $oldPassword = $response['oldPassword'];
+            $newPassword = $response['newPassword'];
+            if (!$user->verifyPassword($oldPassword)) {
+                $this->renderJSON(['error' => 'Invalid password'], 400);
+                die();
+            }
+            $user->setPassword($newPassword);
+        }
+        if (isset($response['firstname']))
+            $user->setFirstname($response['firstname']);
+        if (isset($response['lastname']))
+            $user->setLastname($response['lastname']);
+        if (isset($response['phone']))
+            $user->setPhone($response['phone']);
+
+        $userManager->putOne($user);
+        $this->renderJSON(['message' => 'User updated']);
+        die();
+    }
 }
