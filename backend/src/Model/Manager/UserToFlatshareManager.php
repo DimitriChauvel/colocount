949089d640f7c45a491063cbb6a9a8318b36c938
juroche42 <julesro42@gmail.com>
2024-06-25 20:12:22 +0200

    clean userToFlatshareManager

--- backend/src/Model/Manager/UserToFlatshareManager.php
+++ backend/src/Model/Manager/UserToFlatshareManager.php
@@ -57,20 +57,6 @@ class UserToFlatshareManager extends BaseManager
         return $this->getOne($uniqueId);
     }
 
-    public function post($flatshareId): UserToFlatshare {
-        $body = json_decode(file_get_contents('php://input'), true);
-        $uniqueId = uniqid('UserToFlatshare_');
-        $query = $this->pdo->prepare(<<<EOT
-            INSERT INTO UserToFlatshare (id, user_id, flatshare_id)
-            VALUES (:id, :user_id, :flatshare_id)
-        EOT);
-        $query->bindValue(':id', $uniqueId, \PDO::PARAM_STR);
-        $query->bindValue(':user_id', $body['user_id'], \PDO::PARAM_STR);
-        $query->bindValue('flatshare_id', $flatshareId, \PDO::PARAM_STR);
-        $query->execute();
-        return $this->getOne($uniqueId);
-    }
-
     public function deleteOne(string $id): void {
         $query = $this->pdo->prepare('DELETE FROM UserToFlatshare WHERE user_id = :id');
         $query->bindValue(':id', $id, \PDO::PARAM_STR);
