7983f4091640df567008afbb05e6cf987b670ba3
juroche42 <julesro42@gmail.com>
2024-06-25 18:46:50 +0200

    clean FlatshareController

--- backend/src/Controller/FlatshareController.php
+++ backend/src/Controller/FlatshareController.php
@@ -22,40 +22,6 @@ class FlatshareController extends AbstractController
         $this->renderJSON($data);
     }
 
-    #[Route('/flatshare/id/{id}', name: "get_one_flatshare", methods: ["GET"])]
-    public function getOneFlatshare(string $id) {
-        $flatshares = new FlatshareManager(new PDOFactory());
-        $data = $flatshares->getOne($id);
-
-        header('Content-type: application/json');
-        $this->renderJSON($data);
-    }
-
-    #[Route('/flatshare', name: "post_one_flatshare", methods: ["POST"])]
-    public function postOneFlatshare() {
-        $flatshares = new FlatshareManager(new PDOFactory());
-        $userToFlatshare = new UserToFlatshareManager(new PDOFactory());
-        $data = $flatshares->postOne();
-        $data2 = $userToFlatshare->post($data->getId());
-
-        header('Content-type: application/json');
-        $this->renderJSON($data);
-    }
-
-    #[Route('/flatshare', name: "put_one_flatshare", methods: ["PUT"])]
-    public function putOneFlatshare() {
-        $flatshares = new FlatshareManager(new PDOFactory());
-        $data = $flatshares->putOne();
-        header('Content-type: application/json');
-        $this->renderJSON($data);
-    }
-
-    #[Route('/flatshare/id/{id}', name: "delete_one_flatshare", methods: ["DELETE"])]
-    public function deleteOneFlatshare(string $id) {
-        $flatshares = new FlatshareManager(new PDOFactory());
-        $flatshares->deleteOne($id);
-    }
-
     #[Route('/homepage', name: "homepage", methods: ["GET"])]
     public function homepage() {
         $currentUser = $this->checkJwtAndGetUser();
